a091e1dd887f61d633f87eb8b1b6cadd98ee5cd0
KUSHEEN Dhar <kusheendhar942@gmail.com>
2025-08-22 19:49:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://wellnest-client.onrender.com",
+  credentials: true,
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
@@ -43,7 +46,8 @@ const connectDB = () => {
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+   const PORT = process.env.PORT || 8080;
+   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
